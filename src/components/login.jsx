9ef795c4a43a8f0b3ca0876fbe90dcfd94391992
Tariq-act <khantarikact@gmail.com>
2025-03-30 23:20:20 +0530

    Login UI and State

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { BeatLoader } from "react-spinners";
+import Error from "./error";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormState((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle> Login </CardTitle>
+        <CardDescription>
+          Login to your account if you already have one?
+        </CardDescription>
+        <Error message={"some error"} />
+      </CardHeader>
+      <CardContent className={"space-y-2"}>
+        <div className="space-y-1">
+          <Input
+            name="email"
+            type={"email"}
+            placeholder="Enter Email"
+            onChange={handleInputChange}
+          />
+          <Error message={"some error"} />
+        </div>
+        <div className="space-y-1">
+          <Input
+            name="password"
+            type={"password"}
+            placeholder="Enter Password"
+            onChange={handleInputChange}
+          />
+          <Error message={"some error"} />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button>
+          {true ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default Login;
