e4ac4e898e2359253ad30349dbdf934978b0f4f2
Tariq-act <khantarikact@gmail.com>
2025-04-10 00:37:29 +0530

    redirect to link

--- src/db/apiClicks.js
+++ src/db/apiClicks.js
@@ -1,3 +1,4 @@
+import { UAParser } from "ua-parser-js";
 import supabase from "./supabase";
 
 export async function getClicksForUrl(urlIds) {
@@ -13,3 +14,28 @@ export async function getClicksForUrl(urlIds) {
 
   return data;
 }
+
+const parser = new UAParser();
+
+export const storeClicks = async ({ id, originalUrl }) => {
+  try {
+    const res = parser.getResult();
+    const device = res.device || "desktop"; // Default to desktop if type is not detected
+
+    const response = await fetch("https://ipwhois.app/json/");
+    const { city, country_name: country } = await response.json();
+
+    // Record the click
+    await supabase.from("clicks").insert({
+      url_id: id,
+      city: city,
+      country: country,
+      device: device,
+    });
+
+    // Redirect to the original URL
+    window.location.href = originalUrl;
+  } catch (error) {
+    console.error("Error recording click:", error);
+  }
+};
