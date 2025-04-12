a2b7f89de439b8770fd1fc53da65270ae1127e20
Tariq-act <khantarikact@gmail.com>
2025-04-12 16:42:56 +0530

    Link page

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -1,7 +1,126 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { UrlState } from "@/context";
+import { getClicksForUrl } from "@/db/apiClicks";
+import { deleteUrl, getUrl } from "@/db/apiUrls";
+import useFetch from "@/hooks/use-fetch";
+import { Copy, Download, LinkIcon, Trash } from "lucide-react";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BarLoader, BeatLoader } from "react-spinners";
 
 const LinkPage = () => {
-  return <div>LinkPage</div>;
+  const downloadImage = () => {
+    const imageUrl = url?.qr;
+    const fileName = url?.title;
+
+    const anchor = document.createElement("a");
+    anchor.href = imageUrl;
+    anchor.download = fileName;
+
+    document.body.appendChild(anchor);
+
+    anchor.click();
+
+    document.body.removeChild(anchor);
+  };
+
+  const { id } = useParams();
+  const { user } = UrlState();
+  const navigate = useNavigate();
+
+  const {
+    loading,
+    data: url,
+    fn,
+    error,
+  } = useFetch(getUrl, { id, user_id: user?.id });
+
+  const {
+    loading: loadingStats,
+    data: stats,
+    fn: fnStats,
+  } = useFetch(getClicksForUrl, id);
+
+  const { loading: loadingDelete, fn: fnDelete } = useFetch(deleteUrl, id);
+
+  useEffect(() => {
+    fn();
+    fnStats();
+  }, []);
+
+  if (error) {
+    navigate("/dashboard");
+  }
+
+  let link = "";
+
+  if (url) {
+    link = url?.custom_url ? url?.custom_url : url?.short_url;
+  }
+
+  return (
+    <>
+      {(loading || loadingStats) && (
+        <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
+      )}
+
+      <div className="flex flex-col gap-8 sm:flex-row justify-between">
+        <div className="flex flex-col items-start gap-8 rounded-lg sm:w-2/5">
+          <span className="text-6xl font-extrabold hover:underline cursor-pointer">
+            {url?.title}
+          </span>
+          <a
+            href={`https://www.trimmer.in/${link}`}
+            target="_blank"
+            className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
+          >
+            https://www.trimmer.in/{link}
+          </a>
+          <a
+            href={url?.original_url}
+            target="_blank"
+            className="flex items-center gap-1 hover:underline cursor-pointer"
+          >
+            <LinkIcon className="p-1" />
+            {url?.original_url}
+          </a>
+          <span className="flex items-end font-extralight text-sm">
+            {new Date(url?.created_at).toLocaleString()}
+          </span>
+
+          <div className="flex gap-2">
+            <Button
+              variant={"ghost"}
+              onClick={() =>
+                navigator.clipboard.writeText(
+                  `https://www.trimmer.in/${url?.short_url}`
+                )
+              }
+            >
+              <Copy />
+            </Button>
+            <Button variant={"ghost"} onClick={downloadImage}>
+              <Download />
+            </Button>
+            <Button variant={"ghost"} onClick={() => fnDelete()}>
+              {loadingDelete ? (
+                <BeatLoader size={5} color="white" />
+              ) : (
+                <Trash />
+              )}
+            </Button>
+          </div>
+
+          <img
+            src={url?.qr}
+            className="w-full self-center sm:self-start ring ring-blue-500 p-1 object-contain"
+            alt="qr code"
+          />
+        </div>
+        <div className="sm:w-3/5"></div>
+      </div>
+    </>
+  );
 };
 
 export default LinkPage;
